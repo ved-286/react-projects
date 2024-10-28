d46004e9cb3397b67c899ca4bce86f036cc75931
ved-286 <patelved1921@gmail.com>
2024-10-28 00:39:42 -0700

    change the card component via propes

--- 01Prop-prac/src/App.jsx
+++ 01Prop-prac/src/App.jsx
@@ -2,16 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './component/Card'
 
 function App() {
   const [color, setColor] = useState("olive")
 
   return (
+    <>
    <div className='w-full h-screen bg-slate-600'>
     <div className='text-white flex flex-row justify-center items-center pt-11'>
     <h1>for practice of props</h1>
     </div>
+  
+
+   <Card btntext="ved" des="war is on" />
    </div>
+   </>
   )
 }
 
