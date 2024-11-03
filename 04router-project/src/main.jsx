bc0a104870af836539e8d43869cb843a0b3fd640
ved-286 <patelved1921@gmail.com>
2024-11-03 02:15:13 -0800

    added router provider to Header section

--- 04router-project/src/main.jsx
+++ 04router-project/src/main.jsx
@@ -2,9 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home/Home.jsx'
+import Layout from './Layout.jsx'
+import About from './components/AboutUs/About.jsx'
+import Contact from './components/Contect/Contect.jsx'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    children:[
+      {
+        path:'',
+        element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      },
+      {
+        path:"contact",
+        element:<Contact/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
