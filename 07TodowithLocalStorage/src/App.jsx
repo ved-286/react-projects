fa0256f409a539fc4e51226905c5198ea1088975
unknown <patelved1921@gmail.com>
2024-12-08 09:36:24 -0800

    done with contexts and written methods functionality in app.jsx

--- 07TodowithLocalStorage/src/App.jsx
+++ 07TodowithLocalStorage/src/App.jsx
@@ -1,34 +1,44 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoProvider } from './Contexts'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos,setTodos] =useState([])   
+  
+  
+  const addTodo = (todos)=>{
+         setTodos((prev)=>[{id: Date.now(),...todos},...prev])
+  }
+  
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+  
+
+  const deleteTodo = (id)=>{
+    setTodos((prev)=> prev.filter((todo)=> todo.id === !id))
+  }
+
+  const toggleComplete = ()=>{
+    setTodos((prev)=> prev.map((prevTodo) => prevTodo === id ? {...prevTodo,completed: !prevTodo.completed} :prevTodo))
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
+     <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+    </div>
+    </TodoProvider>
   )
 }
 
